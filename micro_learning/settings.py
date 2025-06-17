77bc1d6a2246690f8e105884cedd3e848695109a
Jérémy Gross <jeremspam1307@gmail.com>
2025-06-17 12:59:41 +0200

    fix settings

--- micro_learning/settings.py
+++ micro_learning/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'micro_learning.settings.AllowAllHostsMiddleware'
+    'settings.AllowAllHostsMiddleware'
 ]
 
 ROOT_URLCONF = 'micro_learning.urls'
